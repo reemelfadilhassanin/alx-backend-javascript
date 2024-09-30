cc7fa20e4dc5b0a29d60d7e8ced57b8390faa068
root <root@Reem.localdomain>
2024-09-30 22:01:26 +0300

    task12

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,25 +1,10 @@
 export default function createIteratorObject(report) {
-    const employees = [];
-    
-    // Collect all employees from the report object
-    for (const department of Object.values(report)) {
-      employees.push(...department);
-    }
-  
-    // Create an iterator
-    return {
-      [Symbol.iterator]() {
-        let index = 0;
-        return {
-          next: () => {
-            if (index < employees.length) {
-              return { value: employees[index++], done: false };
-            } else {
-              return { done: true };
-            }
-          },
-        };
-      },
-    };
+    return (function* _() {
+      for (const department of Object.values(report.allEmployees)) {
+        for (const employee of department) {
+          yield employee;
+        }
+      }
+    }());
   }
   
