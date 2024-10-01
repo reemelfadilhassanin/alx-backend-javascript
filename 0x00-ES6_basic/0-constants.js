4d26fc3a8de7d0f7d72e5337db3f7313cb0e6f4e
root <root@Reem.localdomain>
2024-10-01 05:33:35 +0300

    fix scop

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,16 @@
-// 0-constants.js
 export function taskFirst() {
-    const task = 'I prefer const when I can.'; // Using const
-    return task;
-}
-
-export function getLast() {
-    return ' is okay';
-}
-
-export function taskNext() {
-    let combination = 'But sometimes let'; // Using let
-    combination += getLast();
-    return combination;
-}
+	const task = 'I prefer const when I can.';
+	return task;
+  }
+  
+  export function getLast() {
+	return ' is okay';
+  }
+  
+  export function taskNext() {
+	let combination = 'But sometimes let';
+	combination += getLast();
+  
+	return combination;
+  }
+  
