a8f7ea45db7643422c22f0e925bbf8269af286fc
root <root@Reem.localdomain>
2024-10-02 19:02:04 +0300

    first promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+// 0-promise.js
+const getResponseFromAPI = () => {
+    return new Promise((resolve) => {
+      // You can simulate an asynchronous operation here if needed
+      resolve();
+    });
+  };
+  
+  export default getResponseFromAPI;
+  
