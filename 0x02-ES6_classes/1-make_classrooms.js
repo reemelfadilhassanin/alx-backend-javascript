3a935d10e2c234093d54495420dd0b4cc084bfe7
root <root@Reem.localdomain>
2024-10-03 14:37:42 +0300

    create classroom

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,6 @@
 // 1-make_classrooms.js
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-    return [
-        new ClassRoom(19),
-        new ClassRoom(20),
-        new ClassRoom(34)
-    ];
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
