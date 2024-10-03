c6942c6702a2a17a435b53f0d3bde6f50eddc964
root <root@Reem.localdomain>
2024-10-03 22:37:15 +0300

    task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,53 @@
+// 2-hbtn_course.js
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = this.validateName(name);
+        this._length = this.validateLength(length);
+        this._students = this.validateStudents(students);
+    }
+
+    validateName(name) {
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        return name;
+    }
+
+    validateLength(length) {
+        if (typeof length !== 'number') {
+            throw new TypeError('Length must be a number');
+        }
+        return length;
+    }
+
+    validateStudents(students) {
+        if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        return students;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newName) {
+        this._name = this.validateName(newName);
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    set length(newLength) {
+        this._length = this.validateLength(newLength);
+    }
+
+    get students() {
+        return this._students;
+    }
+
+    set students(newStudents) {
+        this._students = this.validateStudents(newStudents);
+    }
+}
