6cb6cb18826e9d5bcdd5a6673321cf9f92b0d12d
root <root@Reem.localdomain>
2024-10-04 05:08:06 +0300

    configration  files

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,15 +1,15 @@
 export class HolbertonClass {
-	constructor(year, location) {
-	  this._year = year;
-	  this._location = location;
-	}
+    constructor(year, location) {
+      this._year = year;
+      this._location = location;
+    }
   
-	get year() {
-	  return this._year;
-	}
+    get year() {
+      return this._year;
+    }
   
-	get location() {
-	  return this._location;
-	}
+    get location() {
+      return this._location;
+    }
   }
   
