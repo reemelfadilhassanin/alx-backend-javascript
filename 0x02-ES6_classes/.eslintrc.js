c8138ed04b3d9d941ab3e6bbd9537acc4ddb8373
root <root@Reem.localdomain>
2024-10-04 04:37:38 +0300

    configration  files

--- 0x02-ES6_classes/.eslintrc.js
+++ 0x02-ES6_classes/.eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
   plugins: ['jest'],
@@ -28,7 +28,7 @@ module.exports = {
       'WithStatement',
     ],
   },
-  overrides: [
+  overrides:[
     {
       files: ['*.js'],
       excludedFiles: 'babel.config.js',
