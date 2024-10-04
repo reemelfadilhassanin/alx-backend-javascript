b2c3be91e28d1f6d42308add91b5d27996e38f0a
root <root@Reem.localdomain>
2024-10-04 05:01:38 +0300

    configration  files

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,28 +1,21 @@
 // 6-sky_high.js
-import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        super(sqft); // Call the parent constructor
-        this._floors = this.validateFloors(floors);
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
 
-    validateFloors(floors) {
-        if (typeof floors !== 'number') {
-            throw new TypeError('floors must be a number');
-        }
-        return floors;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  set floors(value) {
+    this._floors = value;
+  }
 
-    get floors() {
-        return this._floors;
-    }
-
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this._floors} floors.`;
-    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
