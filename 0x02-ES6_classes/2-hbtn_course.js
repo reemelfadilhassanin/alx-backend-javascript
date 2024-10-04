f3f7f2702ba61fb2d27ad4719a2fe5762da10d3e
root <root@Reem.localdomain>
2024-10-04 04:51:19 +0300

    configration  files

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,41 +1,41 @@
 // 2-hbtn_course.js
 export default class HolbertonCourse {
-	constructor(name, length, students) {
-	  this.name = name;
-	  this.length = length;
-	  this.students = students;
-	}
+    constructor(name, length, students) {
+      this.name = name;
+      this.length = length;
+      this.students = students;
+    }
   
-	get name() {
-	  return this._name;
-	}
+    get name() {
+      return this._name;
+    }
   
-	set name(name) {
-	  if ((typeof name !== 'string') && !(name instanceof String)) {
-		throw new TypeError('Name must be a string');
-	  }
-	  this._name = name;
-	}
+    set name(name) {
+      if ((typeof name !== 'string') && !(name instanceof String)) {
+        throw new TypeError('Name must be a string');
+      }
+      this._name = name;
+    }
   
-	get length() {
-	  return this._length;
-	}
+    get length() {
+      return this._length;
+    }
   
-	set length(length) {
-	  if ((typeof length !== 'number') && !(length instanceof Number)) {
-		throw new TypeError('Length must be a number');
-	  }
-	  this._length = length;
-	}
+    set length(length) {
+      if ((typeof length !== 'number') && !(length instanceof Number)) {
+        throw new TypeError('Length must be a number');
+      }
+      this._length = length;
+    }
   
-	get students() {
-	  return this._students;
-	}
+    get students() {
+      return this._students;
+    }
   
-	set students(students) {
-	  if (!(students instanceof Array) || !students.every((s) => typeof s === 'string')) {
-		throw new TypeError('Students must be an array of strings');
-	  }
-	  this._students = students;
-	}
+    set students(students) {
+      if (!(students instanceof Array) || !students.every((s) => typeof s === 'string')) {
+        throw new TypeError('Students must be an array of strings');
+      }
+      this._students = students;
+    }
   }
