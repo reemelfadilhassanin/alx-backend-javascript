a69bcc61d23ae7896ef0182c5e9615e1cd6e84c3
root <root@Reem.localdomain>
2024-10-09 16:02:51 +0300

    task 5

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,55 @@
+// Interfaces
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+// Classes
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
+
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+
+    workDirectorTasks(): string {
+        return "Getting to director tasks";
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return "Cannot work from home";
+    }
+
+    getCoffeeBreak(): string {
+        return "Cannot have a break";
+    }
+
+    workTeacherTasks(): string {
+        return "Getting to work";
+    }
+}
+
+// Function
+function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === "number" && salary < 500) {
+        return new Teacher();
+    } else {
+        return new Director();
+    }
+}
+
+// Example Usage
+console.log(createEmployee(200)); // Outputs: Teacher
+console.log(createEmployee(1000)); // Outputs: Director
+console.log(createEmployee('$500')); // Outputs: Director
