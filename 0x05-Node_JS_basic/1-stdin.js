679e06a7916f637401bb47da86ff34e00dfeb3f9
root <root@Reem.localdomain>
2024-11-26 16:23:59 +0300

    add inteactive stdn for user

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,17 +3,10 @@
 // Display the prompt message
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Read the user input
-process.stdin.on('data', (input) => {
-  // Trim the input to remove any extra whitespace
-  const name = input.toString().trim();
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
 
-  // Display the input name
-  console.log(`Your name is: ${name}`);
-
-  // Display the closing message
-  console.log('This important software is now closing');
-
-  // End the process
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
